6b9d20a5f2fbd14ba3e219be58d0d0d4819b6f1d
Walter C. Pettus <pettus@uw.edu>
2018-04-06 11:22:43 -0700

    Reordering functions and adding magnetometer back

--- Arduino-magnetometer.ino
+++ Arduino-magnetometer.ino
@@ -5,19 +5,118 @@
 
 char ssid[] = "dem nets"; //  your network SSID (name)
 char pass[] = "SECRET_PASS";    // your network password (use for WPA, or use as key for WEP)
-
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-
-int status = WL_IDLE_STATUS;
-
 WiFiServer server(23);
-
 boolean alreadyConnected = false; // whether or not the client was connected previously
 
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(11111);
 
-void displaySensorDetails(void)
-{
+boolean debug = true; // verbose serial printing
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  configureWiFi();
+  server.begin();
+  
+  configureMag();
+}
+
+void loop() {
+  // wait for a connected client:
+  WiFiClient client = server.available();
+  if (client) {
+    if (debug) Serial.println("Client connected");
+    // when the client sends the first byte, say hello:
+    if (client.available()>0) {
+      if (!alreadyConnected) {
+        // clean out the input buffer:
+        client.flush();
+        Serial.println("We have a new client");
+        client.println("Hello, client!");
+        displayMenu(&client);
+        alreadyConnected = true;
+      }
+      //char input = displayMenu(&client);
+      char input = client.read();
+      
+      switch(input) {
+        case '1':
+          client.println("do stuff");
+          break;
+        case '2':
+          client.println("Resetting sensor not yet implemented");
+          break;
+        default:
+          return;
+      }
+
+    }
+  }
+  else {
+   alreadyConnected = false;
+   client.stop();
+   if (debug) Serial.println("Purging connections");
+  }
+  if (debug) Serial.println("finished loop");
+  delay(5000);
+}
+
+
+
+void configureWiFi() {
+  Serial1.begin(115200); // speed must match with BAUDRATE_COMMUNICATION setting in firmware config.h
+  WiFi.init(&Serial1);
+  WiFi.resetESP(); // to clear 'sockets' after sketch upload
+  delay(3000);
+  Serial.println("ESP reset");
+  
+  // attempt to connect to Wifi network:
+  int status = WL_IDLE_STATUS;
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(3000);  // wait 5 seconds for connection:
+  }
+  
+  // print connection information for network:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("signal strength (RSSI):");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
+}
+
+
+void configureMag() {
+  /* Initialise the sensor */
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while(1);
+  }
+  
+  //Display some basic information on this sensor 
+  displayMagDetails();
+}
+
+
+void displayMenu( WiFiClient *client) {
+  
+  client->println("Select an option: ");
+  client->println(" (1) Display sensor readings");
+  client->println(" (2) Reset sensor");
+}
+
+
+void displayMagDetails() {
   sensor_t sensor;
   mag.getSensor(&sensor);
   Serial.println("------------------------------------");
@@ -29,79 +128,10 @@ void displaySensorDetails(void)
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
   Serial.println("------------------------------------");
   Serial.println("");
-  delay(500);
 }
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-}
-
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("initialized");
-
-  Serial1.begin(115200); // speed must match with BAUDRATE_COMMUNICATION setting in firmware config.h
-  WiFi.init(&Serial1);
-  WiFi.resetESP(); // to clear 'sockets' after sketch upload
-  delay(3000);
-  Serial.println("WiFi reset");
-
-  //Check if communication with wifi module has been established
-  if (WiFi.status() == WL_NO_WIFI_MODULE_COMM) {
-    Serial.println("Communication with WiFi module not established.");
-    while (true); // don't continue:
-  }
-
-  // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  
-  //you're connected now, so print out the status:
-  printWifiStatus();
-  
-  // start the server:
-  server.begin();
-  
-  // Initialise the sensor goes here if not in loop
-  
-  
-  
-  //Display some basic information on this sensor 
-  //server.write("displaySensorDetails()");
-}
-
-void displayMenu( WiFiClient *client) {
-  
-  client->println("Select an option: ");
-  client->println(" (1) Display sensor readings");
-  client->println(" (2) Reset sensor");
-  }
-
-void displaySensorReadings( WiFiClient *client) {
+void displayMagReadings( WiFiClient *client) {
   //* Get a new sensor event*/
   sensors_event_t event; 
   mag.getEvent(&event);
@@ -134,65 +164,7 @@ void displaySensorReadings( WiFiClient *client) {
   float headingDegrees = heading * 180/M_PI; 
   
   Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-  
+  client->println(headingDegrees);
+
   delay(1000);
 }
-
-
-void loop() {
-  // wait for a new client:
-  WiFiClient client = server.available();
-  // when the client sends the first byte, say hello:
-  if (client.available()>0) {
-    if (!alreadyConnected) {
-      // clean out the input buffer:
-      client.flush();
-      Serial.println("We have a new client");
-      client.println("Hello, client!");
-     displayMenu(&client);
-      alreadyConnected = true;
-    }
-    // Initialise the sensor
-  if(!mag.begin())
-  {
-    // There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
-    while(1);
-  }
-    //char input = displayMenu(&client);
-    char input = client.read();
-    
-    switch(input) {
-      case '1':
-        displaySensorReadings(&client);
-        break;
-      case '2':
-        client.println("Resetting sensor not yet implemented");
-        break;
-      default:
-        return;
-      /*case ' ':
-        return;*/
-      /*default:
-        client.println("Invalid option.");
-        client.println(input);
-        break;*/
-    }
-    
-    /*
-    if (client.available() > 0) {
-      // read the bytes incoming from the client:
-      char thisChar = client.read();
-      // echo the bytes back to the client:
-      server.write(thisChar);
-      // echo the bytes to the server as well:
-      Serial.write(thisChar);
-    }*/
-  }
-  else {
-   alreadyConnected = false;
-  }
-}
-
-
-
