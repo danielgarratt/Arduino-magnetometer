f4335701522675e33466b41ed6c28ccece9744e2
Elliot Phillips <erphillips28@gmail.com>
2018-06-04 15:16:39 -0700

    magnetometer code added. runs with prints readings

--- esp-ethernet/esp-ethernet.ino
+++ esp-ethernet/esp-ethernet.ino
@@ -6,6 +6,17 @@
 #include <ETH.h>
 
 static bool eth_connected = false;
+/*
+IPAddress local_IP(192, 168, 1, 205);
+IPAddress gateway(192, 168, 1, 254);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8); //optional
+*/
+// Adafruit_Sensor - Version: Latest 
+#include <Adafruit_Sensor.h>
+
+// Adafruit_HMC5883_Unified - Version: Latest 
+#include <Adafruit_HMC5883_U.h>
 
 void WiFiEvent(WiFiEvent_t event)
 {
@@ -64,18 +75,87 @@ void testClient(const char * host, uint16_t port)
   client.stop();
 }
 
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(11111);
+
+void displaySensorDetails()
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
 void setup()
 {
   Serial.begin(115200);
   WiFi.onEvent(WiFiEvent);
   ETH.begin();
+
+    Serial.begin(57600);
+  Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
 }
+
 
 
 void loop()
 {
-  if (eth_connected) {
+ /* if (eth_connected) {
     testClient("google.com", 80);
   }
   delay(10000);
+*/
+    /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+
+  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  
+  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
+  // Find yours here: http://www.magnetic-declination.com/
+  // Seattle is: +15* 45' E, which is ~15 Degrees, or (which we need) 0.26 radians
+  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
+  float declinationAngle = 0.26;
+  heading += declinationAngle;
+  
+  // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+    
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+   
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/M_PI; 
+  
+  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+  
+  delay(500);
 }
+
