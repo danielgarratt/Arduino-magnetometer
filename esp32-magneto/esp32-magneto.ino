d91840a5143cb687b9c7cbe67cd3a6270adb550d
Elliot Phillips <erphillips28@gmail.com>
2018-06-25 16:50:37 -0700

    ethernet example

--- esp32-magneto/esp32-magneto.ino
+++ esp32-magneto/esp32-magneto.ino
@@ -1,85 +1,16 @@
-#include <MechaQMC5883.h>
+//#include <MechaQMC5883.h>
 
-/*
-    This sketch shows the Ethernet event usage
-//assign static ip
-//include telnet stuff
-*/
-#include <ETH.h>
+//#include <WiFiLink.h>
 
-static bool eth_connected = false;
-/*
-IPAddress local_IP(192, 168, 1, 205);
-IPAddress gateway(192, 168, 1, 254);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8); //optional
-*/
 // Adafruit_Sensor - Version: Latest 
 #include <Adafruit_Sensor.h>
 
 // Adafruit_HMC5883_Unified - Version: Latest 
 #include <Adafruit_HMC5883_U.h>
 
-void WiFiEvent(WiFiEvent_t event)
-{
-  switch (event) {
-    case SYSTEM_EVENT_ETH_START:
-      Serial.println("ETH Started");
-      //set eth hostname here
-      ETH.setHostname("esp32-ethernet");
-      break;
-    case SYSTEM_EVENT_ETH_CONNECTED:
-      Serial.println("ETH Connected");
-      break;
-    case SYSTEM_EVENT_ETH_GOT_IP:
-      Serial.print("ETH MAC: ");
-      Serial.print(ETH.macAddress());
-      Serial.print(", IPv4: ");
-      Serial.print(ETH.localIP());
-      if (ETH.fullDuplex()) {
-        Serial.print(", FULL_DUPLEX");
-      }
-      Serial.print(", ");
-      Serial.print(ETH.linkSpeed());
-      Serial.println("Mbps");
-      eth_connected = true;
-      break;
-    case SYSTEM_EVENT_ETH_DISCONNECTED:
-      Serial.println("ETH Disconnected");
-      eth_connected = false;
-      break;
-    case SYSTEM_EVENT_ETH_STOP:
-      Serial.println("ETH Stopped");
-      eth_connected = false;
-      break;
-    default:
-      break;
-  }
-}
-
-void testClient(const char * host, uint16_t port)
-{
-  Serial.print("\nconnecting to ");
-  Serial.println(host);
-
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    return;
-  }
-  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
-  while (client.connected() && !client.available());
-  while (client.available()) {
-    Serial.write(client.read());
-  }
-
-  Serial.println("closing connection\n");
-  client.stop();
-}
-
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(11111);
 
-void displaySensorDetails()
+void displaySensorDetails(void)
 {
   sensor_t sensor;
   mag.getSensor(&sensor);
@@ -95,12 +26,8 @@ void displaySensorDetails()
   delay(500);
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  WiFi.onEvent(WiFiEvent);
-  ETH.begin();
-
+void setup(void) {
+   
     Serial.begin(57600);
   Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
   
@@ -116,19 +43,12 @@ void setup()
   displaySensorDetails();
 }
 
-
-
-void loop()
-{
- /* if (eth_connected) {
-    testClient("google.com", 80);
-  }
-  delay(10000);
-*/
+void loop(void) {
+    
     /* Get a new sensor event */ 
   sensors_event_t event; 
   mag.getEvent(&event);
-  
+ 
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
   Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
   Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
@@ -160,4 +80,3 @@ void loop()
   
   delay(500);
 }
-
